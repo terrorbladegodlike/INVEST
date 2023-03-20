91bf7037e8a07d54c6d4215529cccf9f5cb55fd0
Mihail <ternovetchii.2002@mail.ru>
2023-03-21 00:45:15 +0200

    added full block right, addaptive mobile and dark theme

--- script.js
+++ script.js
@@ -25,4 +25,30 @@ new Chart(chart, {
    }
 });
 
+// Menu or hide sidebar
+
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sidebar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () => {
+   sidebar.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+   sidebar.style.display = 'none';
+});
+
+// change theme
+
+const themeBtn = document.querySelector('.theme__btn');
+
+themeBtn.addEventListener('click', () => {
+   document.body.classList.toggle('dark__theme');
+
+   themeBtn.querySelector('ion-icon:first-child').classList.toggle('active');
+   themeBtn.querySelector('ion-icon:last-child').classList.toggle('active');
+});
+
+
 
